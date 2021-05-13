74112341d5b30cff6a0b60a3d933d3f7cc26a668
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-05-13 18:38:11 +0900

    add alarm

--- config/config.exs
+++ config/config.exs
@@ -3,5 +3,6 @@ import Config
 config :honeko_pack, HonekoPack.Scheduler,
   jobs: [
     {"31 21 * * *", {HonekoPack, :run, []}},
-    {"50 23 * * *", {HonekoPack, :run, []}}
+    {"50 23 * * *", {HonekoPack, :run, []}},
+    {"50 3 * * *", {HonekoPack, :run, []}}
   ]
